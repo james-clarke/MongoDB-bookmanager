890531590bdc315628606c26dc730359961422f2
James Clarke <james.clarke@tcu.edu>
2023-11-29 20:15:32 -0600

    feat: search book based on price range

--- book_dao.py
+++ book_dao.py
@@ -14,3 +14,8 @@ def findByTitle(book_title):
 def findByPublisher(book_publisher):
     results = collection.find({'published_by': book_publisher})
     return results
+
+
+def findByPriceRange(min_price, max_price):
+    results = collection.find({'price': {'$gte': min_price, '$lte': max_price}})
+    return results
--- menu.py
+++ menu.py
@@ -25,7 +25,6 @@ def search_all_books():
     # abstract the retrieval of data from 
     # a data resource such as a database.
     results = book_dao.findAll()
-
     # Display results
     print("The following are the ISBNs and titles of all books.")
     for item in results:
@@ -49,7 +48,6 @@ def search_by_title():
 def search_by_publisher():
     publisher = input("Enter the publisher name to search for books:\n")
     results = list(book_dao.findByPublisher(publisher))
-
     # Display results
     if len(results) > 0:
         print("Books published by", publisher, ":")
@@ -57,6 +55,24 @@ def search_by_publisher():
             print(book['ISBN'], book['title'])
     else:
         print("No books found for the publisher:", publisher)
+    print("The end.")
+
+
+def search_by_price_range():
+    try:
+        min_price = float(input("Enter the minimum price:\n"))
+        max_price = float(input("Enter the maximum price:\n"))
+    except ValueError:
+        print("Please enter valid numbers for price.")
+        return
+    results = list(book_dao.findByPriceRange(min_price, max_price))
+    # Display results
+    if len(results) > 0:
+        print(f"Books in the price range {min_price} to {max_price}:")
+        for book in results:
+            print(book['ISBN'], book['title'], "- Price:", book['price'])
+    else:
+        print(f"No books found in the price range {min_price} to {max_price}.")
     print("The end.")
 
 
@@ -74,7 +90,7 @@ def print_search_menu():
     print()
     print("Please make a selection")
     for key in search_menu_options.keys():
-        print (str(key)+'.', search_menu_options[key], end = "  ")
+        print(str(key)+'.', search_menu_options[key], end="  ")
     print()
     print("The end of search menu options")
     print()
@@ -114,6 +130,9 @@ def option5():
     elif option == 3:
         print('Search Option 3: search books by publisher.')
         search_by_publisher()
+    elif option == 4:
+        print('Search Option 3: search books by price range.')
+        search_by_price_range()
 
 
 if __name__ == '__main__':
